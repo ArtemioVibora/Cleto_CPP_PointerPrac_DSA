fa33034252a00f6d3de07efe60e8ecf15d7204ac
AltruisticDeveloper <94912927+ArtemioVibora@users.noreply.github.com>
2025-07-02 21:12:48 +0800

    DSA PRAC POINTER 1

---
+++ pointerPrac1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    x = 15;
+    
+    cout << x << endl; 
+    cout << &x << endl; //Address of operator which its output will be 0x --- something ---
+    
+    //0x is just a notation that it is in the form of hexadecimal
+    
+    return 0;
+    
+}
